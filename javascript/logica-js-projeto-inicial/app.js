e6ad3ffb9e7a53751622da9be72cfab8f4b83eb5
matheuspvbastos <matheusvbastos@yahoo.com.br>
2024-07-29 20:17:26 -0300

    primeiros passos no javascript

---
+++ javascript/logica-js-projeto-inicial/app.js
@@ -0,0 +1,9 @@
+alert('Boas-vindas ao jogo do número secreto');
+let numeroSecreto = 5;
+let chute = prompt('Escolha um número entre 1 e 10');
+
+if (chute == numeroSecreto) {
+        console.log("Isso aí! você descobriu o número secreto (5)");
+} else {
+    alert ('Você errou :(')
+}
