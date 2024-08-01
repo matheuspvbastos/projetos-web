708216b14cc4855cb87affed5fde2439928ca2b5
matheuspvbastos <matheusvbastos@yahoo.com.br>
2024-08-01 20:00:28 -0300

    mais coisa de js

--- javascript/logica-js-projeto-inicial/app.js
+++ javascript/logica-js-projeto-inicial/app.js
@@ -1,11 +1,24 @@
 alert('Boas-vindas ao jogo do número secreto');
 let numeroSecreto = 5;
-console.log(numeroSecreto)
+console.log(numeroSecreto);
 let chute = prompt('Escolha um número entre 1 e 10');
+let tentativas = 1;
 
-//se chute for igual ao número secreto
-if (chute == numeroSecreto) {
-        alert('Isso aí! você descobriu o número secreto ' + numeroSecreto );
-} else {
-    alert ('Você errou :(')
+// enquanto chute nao for irmao ao numeroSecreto
+while(chute != numeroSecreto){
+    chute = prompt('Escolha um número entre 1 e 10');
+
+    //se chute for igual ao número secreto
+    if (chute == numeroSecreto) {
+        alert (`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`)
+        
+    } else {
+        if (chute > numeroSecreto)
+            alert ('o número secreto é menor que ' + chute);
+        else {
+            alert ('o número secreto é maoir que ' + chute);
+        }
+        tentativas++ ;
+    }
+
 }
