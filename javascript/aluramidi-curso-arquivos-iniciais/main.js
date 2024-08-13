5f735ac0dda949562099eee8252ba1b31ca63796
matheuspvbastos <matheusvbastos@yahoo.com.br>
2024-08-13 20:55:41 -0300

    mais sobre js

--- javascript/aluramidi-curso-arquivos-iniciais/main.js
+++ javascript/aluramidi-curso-arquivos-iniciais/main.js
@@ -1,1 +1,10 @@
-document.querySelector('.tecla_pom');
+function tocaSomPom() {
+    document.querySelector('#som_tecla_pom').play();
+}
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
+// OUTRA MANEIRA 
+//document.querySelector('.tecla_pom').addEventListener('click', tocaSomPom);
+
+function tocaSomClap() {
+    document.querySelector('#som_tecla_clap').play();
+}
