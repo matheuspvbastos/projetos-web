72a2f55920d58000e3db2ddf662b185e3e0f707c
matheuspvbastos <matheusvbastos@yahoo.com.br>
2024-08-14 20:30:24 -0300

    referencias e listas no js

--- javascript/aluramidi-curso-arquivos-iniciais/main.js
+++ javascript/aluramidi-curso-arquivos-iniciais/main.js
@@ -1,10 +1,11 @@
+
 function tocaSomPom() {
     document.querySelector('#som_tecla_pom').play();
 }
 document.querySelector('.tecla_pom').onclick = tocaSomPom;
-// OUTRA MANEIRA 
-//document.querySelector('.tecla_pom').addEventListener('click', tocaSomPom);
+// OUTRA MANEIRA document.querySelector('.tecla_pom').addEventListener('click', tocaSomPom);
 
-function tocaSomClap() {
-    document.querySelector('#som_tecla_clap').play();
-}
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+listaDeTeclas[0].onclick = tocaSomPom
+
