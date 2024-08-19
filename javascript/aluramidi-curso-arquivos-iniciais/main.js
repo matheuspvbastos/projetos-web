9706ffa7dbb8aa0a51f1612b1ab40a8134f4b6f6
matheuspvbastos <matheusvbastos@yahoo.com.br>
2024-08-19 20:42:00 -0300

    tentando aprender mais o js

--- javascript/aluramidi-curso-arquivos-iniciais/main.js
+++ javascript/aluramidi-curso-arquivos-iniciais/main.js
@@ -1,11 +1,26 @@
-
-function tocaSomPom() {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom(idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
-// OUTRA MANEIRA document.querySelector('.tecla_pom').addEventListener('click', tocaSomPom);
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-listaDeTeclas[0].onclick = tocaSomPom
+let contador = 0;
 
+//enquanto
+while (contador < listaDeTeclas.length) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    //template string
+    const idAudio = `#som_${instrumento}`;
+
+    //console.log(idAudio);
+
+    tecla.onclick = function(){
+        tocaSom(idAudio);
+    }
+    
+    contador =  contador + 1;
+
+    //console.log(contador);
+}
