982460b2c5c8437cf820d32ea79c94f7fb950ae3
matheuspvbastos <matheusvbastos@yahoo.com.br>
2024-08-20 20:32:55 -0300

    tentando recomeçar

--- javascript/logica-js-projeto-inicial/app.js
+++ javascript/logica-js-projeto-inicial/app.js
@@ -1,30 +1,34 @@
+//notificação na tela (mensagem da notificação)
 alert('Boas-vindas ao jogo do número secreto');
-let numeroSecreto = 5;
-console.log(numeroSecreto);
-let chute = prompt('Escolha um número entre 1 e 10');
-let tentativas = 1;
 
-// enquanto chute nao for irmao ao numeroSecreto
-while(chute != numeroSecreto){
+//declaração de variavél
+let numeroSecreto = 5;
+
+//imprimir a declaração numeroSecreto que no caso é o 5
+console.log(numeroSecreto);
+
+//declaração para armazenar o chute
+let chute
+
+//enquanto chute não for igual ao numeroSecreto
+while(chute != numeroSecreto) {
+    //para pessoa escrever ou responder o que estiver dentro do prompt
     chute = prompt('Escolha um número entre 1 e 10');
 
-    //se chute for igual ao número secreto
+    //SE chute for igual ao número secreto
     if (chute == numeroSecreto) {
-        
-        
-    } else {
+        alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto}`);
+        //a variavel junto com a string deverá ser colocada entre crases
+
+    }//SENÃO 
+    else {                      
+        //SE
         if (chute > numeroSecreto)
-            alert ('o número secreto é menor que ' + chute);
-        else {
-            alert ('o número secreto é maoir que ' + chute);
-        }
-        tentativas++ ;
+            alert (`O número secreto é menor que ${chute}`);
+        
+            //SENÃO
+            else {
+            alert (`O número secreto é maior que ${chute}`);
+        }   
     }
-
-}
-
-if (tentativas > 1) {
-    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
-} else {
-    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa.`);
 }
