d61ce6b8b155eebf147a0693d811465ea64fbd2f
matheuspvbastos <matheusvbastos@yahoo.com.br>
2024-10-03 20:53:17 -0300

    mais de js

---
+++ javascript/cursoJS/aulas/ex008.js
@@ -0,0 +1,8 @@
+var vel = 120
+console.log(`A velocidade do seu carro é ${vel} Km/h `)
+
+if (vel > 80){
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+} //condição simples
+
+console.log('Use sempre cinto de segurança!')
