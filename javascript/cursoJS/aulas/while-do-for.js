21db0a12e9bae5df66c77ccb0720b66f5bc1d4b2
matheuspvbastos <matheusvbastos@yahoo.com.br>
2024-11-26 20:25:53 -0300

    aprendendo o for

---
+++ javascript/cursoJS/aulas/while-do-for.js
@@ -0,0 +1,21 @@
+/*var a = 1 
+while (a <= 6){ // faça isso até ser mentira
+    console.log (`Passo ${a}`) 
+    a++
+}
+
+var b = 1 
+do { // faça 
+    console.log(`Passo ${b}`)
+    b++
+} while (b <= 6) //equanto isso for verdade
+*/
+console.log('COMEÇOU')
+for (var c = 1 ; c <= 6; c++) {
+    //variavel, teste lógico e incrementação
+    console.log (c)
+
+}
+console.log('FIM')
+
+
