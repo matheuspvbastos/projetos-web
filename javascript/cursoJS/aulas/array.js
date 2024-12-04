255c4d006f8717545087d2ddf94e53ac5f774c77
matheuspvbastos <matheusvbastos@yahoo.com.br>
2024-12-04 20:09:55 -0300

    entendendo array

---
+++ javascript/cursoJS/aulas/array.js
@@ -0,0 +1,7 @@
+let num = [5, 8, 9 ,7 ,2]
+num.push(1) // coloca o conteudo em ultimo lugar criando um elemento para ele 
+num.sort() // colocar em ordem crescente 
+
+console.log(`minha primeira lista: ${num}`)
+console.log(`o vetor tem ${num.length} posições`)
+console.log(`o primeiro valor do vetor é ${num[0]}`)
