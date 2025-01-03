c54286b47827f95b39ead2b560653bbb508b463f
matheuspvbastos <matheusvbastos@yahoo.com.br>
2025-01-03 18:16:22 -0300

    mais sobre js

--- javascript/js-curso-2-aula1/app.js
+++ javascript/js-curso-2-aula1/app.js
@@ -1,28 +1,60 @@
- /*let titulo = document.querySelector('h1')
+/*let titulo = document.querySelector('h1')
  titulo.innerHTML = 'Jogo do Nº Secreto'
 
  let p = document.querySelector('p')
  p.innerHTML = 'Escolha um número entre 1 e 10'
 */
-
-numeroSecreto = numeroAleatorio()
+let numeroSecreto = numeroAleatorio();
+let tentativas = 1;
 
 // boa prática utilizando function
-function exibirNaTela(tag, texto){
-    let campo = document.querySelector(tag)
-    campo.innerHTML = texto
+function exibirNaTela(tag, texto) {
+  let campo = document.querySelector(tag);
+  campo.innerHTML = texto;
 }
 
-exibirNaTela('h1', 'Jogo do Nº Secreto')
-exibirNaTela('p', 'Escolha um número entre 1 e 10:')
+function mensagemInicial(){
+   exibirNaTela("h1", "Jogo do Nº Secreto"); //chama a função
+   exibirNaTela("p", "Escolha um número entre 1 e 10:");
+}
+
+mensagemInicial(); //chama a função
+
+function verificarChute() {
+  let chute = document.querySelector("input").value;
+  if (chute == numeroSecreto) {
+    let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa' 
+    let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}`;
+    exibirNaTela("h1", "Você acertou!");
+    exibirNaTela("p", mensagemTentativas);
+
+    document.querySelector('#reiniciar').removeAttribute('disabled') //remove o atributo do botão Novo Jogo caso o usuário acerte o nº secreto
+
+  } else {
+    if (chute > numeroSecreto) {
+      exibirNaTela("p", "O número é menor");
+    } else {
+      exibirNaTela("p", "O número é maior");
+    }
+    tentativas++;
+    limparCampo(); //função para limpar o input apos o erro
+  }
+}
 
 
- function verificarChute(){
-    let chute = document.querySelector('input').value
-    console.log( chute == numeroSecreto)
+function numeroAleatorio() {
+  return parseInt(Math.random() * 10 + 1);
+}
 
- }
+function limparCampo(){
+   chute = document.querySelector('input')
+   chute.value = ''
+}
 
- function numeroAleatorio(){
-    return parseInt(Math.random() * 10 + 1)
- }
+function reiniciarJogo(){
+   numeroSecreto = numeroAleatorio();
+   tentativas = 1;
+   limparCampo()
+   mensagemInicial();
+   document.querySelector('#reiniciar').setAttribute('disabled', true)
+}
