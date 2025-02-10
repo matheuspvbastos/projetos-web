cc538273201cdd2f98add376b139b142b6dc2e96
matheuspvbastos <matheuspvbastos@gmail.com>
2025-02-10 20:18:22 -0300

    aprendendo eventos

--- javascript/lista-de-compras/index.js
+++ javascript/lista-de-compras/index.js
@@ -1,2 +1,8 @@
 const inputItem = document.querySelector('#input-item');
 
+const botaoAdicionar = document.querySelector('#adicionar-item');
+
+botaoAdicionar.addEventListener("click", (evento) =>{ 
+    evento.preventDefault(); // previbe que a página recarregue ou vá para outra página
+    
+})
